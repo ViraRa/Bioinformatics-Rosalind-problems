77965eac06d329b5788412cc5a5364c206b93a9c
Viraj Rana <vir39@pitt.edu>
2020-04-29 12:53:09 -0400

    Update Project 4.py

--- Project 4.py
+++ Project 4.py
@@ -1,21 +1,21 @@
-# class Wabbits:
+class Wabbits:
 
-#     a_sub_0 = 0
-#     a_sub_1 = 1
-#     memo = []
+    a_sub_0 = 0
+    a_sub_1 = 1
+    memo = []
 
-#     def __init__(self):
+    def __init__(self):
 
-#         self.memo.append(self.a_sub_0) #index 0
-#         self.memo.append(self.a_sub_1) #index 1
+        self.memo.append(self.a_sub_0) #index 0
+        self.memo.append(self.a_sub_1) #index 1
 
-#     def rabbit_recurrence(self, n, k):
+    def rabbit_recurrence(self, n, k):
 
-#         for x in range(2, n):
+        for x in range(2, n):
 
-#             self.memo.append(k * self.memo[x-2] + (self.memo[x-1]))
+            self.memo.append(k * self.memo[x-2] + (self.memo[x-1]))
 
-#         return self.memo
+        return self.memo
     
-# obj = Wabbits()
-# print(obj.rabbit_recurrence(32,5))
+obj = Wabbits()
+print(obj.rabbit_recurrence(32,5))
