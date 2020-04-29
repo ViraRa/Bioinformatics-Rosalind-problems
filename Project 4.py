e1200322f7d31c4ac977b8327b4e1ae51a73a4da
Viraj Rana <vir39@pitt.edu>
2020-04-29 12:51:41 -0400

    Create Project 4.py

---
+++ Project 4.py
@@ -0,0 +1,21 @@
+# class Wabbits:
+
+#     a_sub_0 = 0
+#     a_sub_1 = 1
+#     memo = []
+
+#     def __init__(self):
+
+#         self.memo.append(self.a_sub_0) #index 0
+#         self.memo.append(self.a_sub_1) #index 1
+
+#     def rabbit_recurrence(self, n, k):
+
+#         for x in range(2, n):
+
+#             self.memo.append(k * self.memo[x-2] + (self.memo[x-1]))
+
+#         return self.memo
+    
+# obj = Wabbits()
+# print(obj.rabbit_recurrence(32,5))
