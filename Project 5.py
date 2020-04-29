85f103a806b3e03e4d521bc46afff7a73cbc0015
Viraj Rana <vir39@pitt.edu>
2020-04-29 12:54:37 -0400

    Create Project 5.py

---
+++ Project 5.py
@@ -0,0 +1,20 @@
+class counting_PM:
+
+    list_0 = []
+    list_1 = []
+    count = 0
+
+    def __init__(self):
+
+        file = open("PM.txt", "r")
+        seq = file.read()
+        string_0, string_1 = seq.split("\n", 2)
+        word = zip(string_0, string_1)
+       
+        for x, y in word:
+            if(x != y):
+                self.count+=1
+
+        print(self.count)
+           
+obj = counting_PM()
