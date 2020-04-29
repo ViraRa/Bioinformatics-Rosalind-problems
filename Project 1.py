8e515f9279a51b4bb8bb8605ab9338f2c3c68f7e
Viraj Rana <vir39@pitt.edu>
2020-04-29 12:42:02 -0400

    Create Project 1.py

---
+++ Project 1.py
@@ -0,0 +1,22 @@
+class counting_DNA_nucleotide:
+
+    string = ""
+
+    def __init__(self):
+
+        try:
+            file = open("sample.txt", "r")
+        except FileNotFoundError:
+            print("File cannot be found")
+
+        for x in file:
+            self.string += x
+
+        num_A = self.string.count("A")
+        num_G = self.string.count("G")
+        num_C = self.string.count("C")
+        num_T = self.string.count("T")
+
+        print(str(num_A) + " " + str(num_C) + " " + str(num_G) +  " " + str(num_T))
+
+obj = counting_DNA_nucleotide()
